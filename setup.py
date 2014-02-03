62d8e3c1ab996367444c7f48430560c9d2f8b201
Alex Kang <akang95@gmail.com>
2014-02-03 01:02:38 -0800

    Initial commit
    Was published on PyPi last year, but now uploading it on GitHub

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    description='Calculates various operations of matrices.',
+    author='Alex Kang',
+    url='https://pypi.python.org/pypi?:action=display&name=MatrixCalculator',
+    download_url='https://pypi.python.org/pypi?:action=display&name=MatrixCalculator',
+    license='GPL',
+    author_email='akang95@gmail.com',
+    version='0.1.7',
+    packages=['matrixcalc'],
+    scripts=['bin/matrixcalculator'],
+    name='MatrixCalculator'
+)
